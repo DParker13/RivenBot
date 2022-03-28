54b4c1deaa27c345dec9391147641ad921377513
Xonor13 <danielparker134@gmail.com>
2022-03-28 02:02:25 -0400

    Fixed cookies.txt file

--- RunRiven.py
+++ RunRiven.py
@@ -7,6 +7,8 @@ from os import environ
 youtube_dl.utils.bug_reports_message = lambda: ''
 
 ytdl_format_options = {
+    'username': 'meepmeep04@gmail.com',
+    'password': environ["YT_PASSWORD"],
     'format': 'bestaudio/best',
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'restrictfilenames': True,
@@ -18,8 +20,6 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
-    'username': 'meepmeep04@gmail.com',
-    'password': environ["YT_PASSWORD"],
     'cookiefile': 'cookies.txt'
 }
 
--- RunRiven_Local.py
+++ RunRiven_Local.py
@@ -9,6 +9,8 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 YT_PASSWORD = ''
 
 ytdl_format_options = {
+    'username': 'meepmeep04@gmail.com',
+    'password': YT_PASSWORD,
     'format': 'bestaudio/best',
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'postprocessors': [{
@@ -24,8 +26,6 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
-    'username': 'meepmeep04@gmail.com',
-    'password': YT_PASSWORD,
     'cookiefile': 'cookies.txt'
 }
 

cf82c791efeb5e273b825bc7339e7d16c8ef15d3
Xonor13 <danielparker134@gmail.com>
2023-04-06 21:47:19 -0400

    Added more intricate logging

--- RunRiven.py
+++ RunRiven.py
@@ -1,7 +1,6 @@
 import argparse
 from Logger import Logger
 from Riven import Riven
-from YTDL import YTDL
 
 # Command line arguments
 parser = argparse.ArgumentParser(description='Discord bot for the Soft Tacos')
@@ -24,4 +23,4 @@ logger = Logger(base_path=args.logpath, enable_logs=args.enablelogs)
 riven_bot = Riven(logger, args.status, YT_PASSWORD)
 
 riven_bot.loop.create_task(riven_bot.audio_player_task())
-riven_bot.run(DIS_TOKEN)
+riven_bot.run(DIS_TOKEN, log_handler=None)

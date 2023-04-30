fbdb5071e466455fd9f2b3e56a5af40ab87a589f
Xonor13 <danielparker134@gmail.com>
2023-04-30 18:12:58 -0400

    Update OpenAICommands.py
    removed chat history file

--- OpenAICommands.py
+++ OpenAICommands.py
@@ -4,17 +4,10 @@ import openai
 class OpenAICommands:
     chat_history = []
 
-    def __init__(self, client, logger, api_key, chat_file_path):
+    def __init__(self, client, logger, api_key):
         self.client = client
         self.logger = logger
         openai.api_key = api_key
-        self.chat_file_path = chat_file_path
-        self.setup_chat_file(chat_file_path)
-
-    def setup_chat_file(self, chat_file_path):
-        with open(chat_file_path, "a+") as file:
-            self.chat_history = file.readlines()
-        file.close()
 
 
     def addOpenAICommands(self):
@@ -23,15 +16,14 @@ class OpenAICommands:
         async def chat(ctx):
             content = ctx.message.content[5:].strip()
 
+            if len(self.chat_history) >= 500:
+                del self.chat_history[0]
+
             self.logger.print("Chat -", content)
-            self.chat_history.append(str({"role": "user", "content": content}))
+            self.chat_history.append({"role": "user", "content": content})
             completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.chat_history)
             resp = completion.choices[0].message.content.strip("\n").strip()
-            self.chat_history.append(str({"role": "assistant", "content": resp}))
-
-            # Writes user content and chatGPT response to file
-            with open(self.chat_file_path, "w") as file:
-                file.writelines(self.chat_history)
+            self.chat_history.append({"role": "assistant", "content": resp})
 
             self.logger.print("ChatGPT Response -", resp)
             await ctx.send(resp)

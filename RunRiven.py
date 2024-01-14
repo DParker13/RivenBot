0d3782d858ef5b9b0ec7112335683893ac456b66
DParker13 <danielparker134@gmail.com>
2024-01-13 23:11:59 -0500

    added option to reconnect yt_dlp while playing audio

--- RunRiven.py
+++ RunRiven.py
@@ -35,7 +35,8 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
-    'cookiefile': 'cookies.txt'
+    'cookiefile': 'cookies.txt',
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
 }
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 yt_dlp.utils.bug_reports_message = lambda: ''
